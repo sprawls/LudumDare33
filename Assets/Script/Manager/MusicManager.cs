708bd531c3631a4ec5129de03725a61ee2d751fd
sprawls <alex3521@hotmail.fr>
2015-08-23 02:06:04 -0400

    Added music, More structure to folders

---
+++ Assets/Script/Manager/MusicManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    public static MusicManager Instance;
+
+    public AudioClip musicClip;
+    private AudioSource audioSource;
+    
+
+    void Awake() {
+        if (Instance == null) {
+            MusicManager.Instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null) audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.loop = true;
+        audioSource.clip = musicClip;
+        audioSource.Play();
+    }
+
+    void OnDestroy() {
+        if (Instance == this) {
+            Instance = null;
+        }
+    }
+}
