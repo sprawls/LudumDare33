19987bc8c05d4495527d8a2bccf330ecd5a421d6
sprawls <alex3521@hotmail.fr>
2016-07-10 11:57:18 -0400

    Different visuals when in game in inverse world

---
+++ Assets/Game Assets/Script/Utility/ActivationOnInverseWorld.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ActivationOnInverseWorld : MonoBehaviour {
+
+    private static List<ActivationOnInverseWorld> scriptList;
+    private GameObject go;
+
+    public bool ActivateOnInverseWorld = false;
+
+    void Awake() {
+        if (scriptList == null) scriptList = new List<ActivationOnInverseWorld>();
+        scriptList.Add(this);
+        go = gameObject;
+    }
+
+	void Start () {
+        UpdateActivation();
+	}
+
+    void OnDestroy() {
+        if (scriptList.Contains(this)) scriptList.Remove(this);
+    }
+
+    public static void UpdateAll() {
+        foreach (ActivationOnInverseWorld script in scriptList) {
+            script.UpdateActivation();
+        }
+    }
+
+    public void UpdateActivation() {
+        bool inverse = LevelManager.Instance.isInverseWorld() ? true : false;
+        go.SetActive(ActivateOnInverseWorld == inverse);
+    }
+}
---
+++ Assets/Game Assets/Script/Utility/ChangeColorByInverseWorld.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class ChangeColorByInverseWorld : MonoBehaviour {
+
+    public bool ChangeText = false;
+    public bool ChangeMeshRenderer = false;
+    public bool ChangeImage = false;
+
+    private static List<ChangeColorByInverseWorld> colorList;
+
+    public Color normalColor;
+    public Color InverseColor;
+
+    private MeshRenderer meshRendrer;
+    private Text text;
+    private Image image;
+
+	void Awake(){
+        if (colorList == null) colorList = new List<ChangeColorByInverseWorld>();
+        colorList.Add(this);
+    }
+
+    void Start() {
+        if(ChangeText) text = GetComponent<Text>();
+        if(ChangeMeshRenderer) meshRendrer = GetComponent<MeshRenderer>();
+        if (ChangeImage) image = GetComponent<Image>();
+        UpdateColor(0f);
+    }
+
+    void OnDestroy(){
+        if(colorList.Contains(this)) colorList.Remove(this);
+    }
+
+    public static void UpdateAllColor(){
+        foreach (ChangeColorByInverseWorld col in colorList) {
+            col.UpdateColor(2f);
+        }
+    }
+
+    public void UpdateColor(float time){
+        Color TargetColor = LevelManager.Instance.isInverseWorld() ? InverseColor : normalColor;
+
+        if (meshRendrer != null) meshRendrer.material.DOColor(TargetColor, time);
+        if (text != null) text.DOColor(TargetColor, time);
+        if (image != null) image.DOColor(TargetColor, time);
+    }
+}
