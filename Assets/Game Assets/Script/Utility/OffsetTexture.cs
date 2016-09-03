02a8edf6770f421b6f709cee1eab02100d838ac0
sprawls <alex3521@hotmail.fr>
2016-09-03 10:19:51 -0400

    Added a new offset method using UV to avoid create material instances runtime

--- Assets/Game Assets/Script/Utility/OffsetTexture.cs
+++ Assets/Game Assets/Script/Utility/OffsetTexture.cs
@@ -3,31 +3,52 @@ using System.Collections;
 
 
 public class OffsetTexture : MonoBehaviour {
+    private bool UseUVMethod = false;
+
     public float offsetX = 0f;
     public float offsetY = 0f;
 
     public Vector2 offsetRate;
     public bool randomizeStartOffset = false;
 
-    private MeshRenderer mesh;
+    private MeshRenderer _meshRenderer;
+    private Mesh _mesh;
+    private Vector2[] _meshUVs;
 
     void Start() {
 
-        mesh = gameObject.GetComponent<MeshRenderer>();
+        _meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        _mesh = gameObject.GetComponent<MeshFilter>().mesh;
+        _meshUVs = _mesh.uv;
+
         if (randomizeStartOffset) {
-            offsetX = Random.Range(0f, 1f);
-            offsetY = Random.Range(0f, 1f);
-            mesh.material.SetTextureOffset("_MainTex", new Vector2(offsetX, offsetY));
-            mesh.material.SetTextureOffset("_BumpMap", new Vector2(offsetX, offsetY));
+            if (UseUVMethod) {
+                float randomOffsetX = Random.Range(0f, 1f);
+                float randomOffsetY = Random.Range(0f, 1f);
+                for (int i = 0; i < _meshUVs.Length; ++i) {
+                    _meshUVs[i] = new Vector2(_meshUVs[i].x + randomOffsetY, _meshUVs[i].y + randomOffsetY);
+                }
+                _mesh.uv = _meshUVs;
+            } else {
+                offsetX = Random.Range(0f, 1f);
+                offsetY = Random.Range(0f, 1f);
+                _meshRenderer.material.SetTextureOffset("_MainTex", new Vector2(offsetX, offsetY));
+                _meshRenderer.material.SetTextureOffset("_BumpMap", new Vector2(offsetX, offsetY));
+            }
         }
     }
 
 	void Update () {
-        offsetX += offsetRate.x * Time.deltaTime;
-        offsetY += offsetRate.y * Time.deltaTime;
-        mesh.material.SetTextureOffset("_MainTex", new Vector2(offsetX, offsetY));
-        mesh.material.SetTextureOffset("_BumpMap", new Vector2(offsetX, offsetY));
+        if (UseUVMethod) {
+            for (int i = 0; i < _meshUVs.Length; ++i) {
+                _meshUVs[i] = new Vector2(_meshUVs[i].x + (offsetRate.x * Time.deltaTime), _meshUVs[i].y + (offsetRate.y * Time.deltaTime));
+            }
+            _mesh.uv = _meshUVs;
+        } else {
+            offsetX += offsetRate.x * Time.deltaTime;
+            offsetY += offsetRate.y * Time.deltaTime;    
+            _meshRenderer.material.SetTextureOffset("_MainTex", new Vector2(offsetX, offsetY));
+            _meshRenderer.material.SetTextureOffset("_BumpMap", new Vector2(offsetX, offsetY));
+        }
 	}
-
-
 }
