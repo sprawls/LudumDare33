e83ee5c4d4b12923d71bda7cb720ae833a0cb00d
sprawls <alex3521@hotmail.fr>
2016-10-02 16:14:47 -0400

    Added Funding Page on wordls 3  and its inverse version

---
+++ Assets/ProgressBar3D.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProgressBar3D : MonoBehaviour {
+
+    private Transform _innerBar;
+    private Transform _outerBar;
+
+    private float _completionRatio = 0f;
+
+	void Awake () {
+        _innerBar = transform.Find("outerBar");
+        _outerBar = _innerBar.Find("innerBar");
+
+        if (_innerBar == null || _outerBar == null) Debug.LogError("Components of progress bars not found ! Check Hierarchy !");
+        else {
+            StartCoroutine(UpdateUIBar());
+        }
+	}
+
+    IEnumerator UpdateUIBar() {
+        //todo with www
+        _completionRatio = Random.Range(0f, 1f);
+        yield return null;
+
+        UpdateInnerBar();
+    }
+
+    private void UpdateInnerBar() {
+        Mathf.Clamp(_completionRatio, 0f, 1f);
+        //set scale
+        transform.localScale = new Vector3(_completionRatio, 1f, 1f);
+        //set position
+        transform.localPosition = new Vector3((1f - _completionRatio) * -0.5f, 0f,0f);
+    }
+}
